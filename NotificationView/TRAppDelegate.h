3c1a24eb73ea2228a68a9fdb57abe00e7c5db0f5
Danil Komarov <Danil@MacBook-Pro-Danil.local>
2012-09-12 11:10:15 +0400

    Initial Commit

---
+++ NotificationView/TRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRAppDelegate.h
+//  NotificationView
+//
+//  Created by Danil Komarov on 12.09.12.
+//  Copyright (c) 2012 Two Rules. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NotificationView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NotificationView
+//
+//  Created by Danil Komarov on 12.09.12.
+//  Copyright (c) 2012 Two Rules. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRAppDelegate class]));
+    }
+}
